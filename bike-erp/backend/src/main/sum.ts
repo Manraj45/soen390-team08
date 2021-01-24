1707ad6463281fc4ef766f5782b2257d1b3ac587
Simon Lim <simonlim898@hotmail.com>
2021-01-24 18:53:23 -0500

    #14 Removed test file

--- bike-erp/backend/src/main/sum.ts
+++
@@ -1,9 +0,0 @@
-let myAdd: (baseValue: number, increment: number) => number = function (
-    x: number,
-    y: number
-  ): number {
-    return x + y;
-  };
-
-  module.exports = myAdd
-  console.log(myAdd(1,7))
