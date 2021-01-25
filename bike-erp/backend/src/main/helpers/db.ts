c0af33c87ba2fb3bba223061b0bfd3df388c83f6
Sam <huangs.wk.0508@gmail.com>
2021-01-25 15:55:36 -0500

    Updated db.js file for the db connection

--- bike-erp/backend/src/main/helpers/db.ts
+++ bike-erp/backend/src/main/helpers/db.ts
@@ -3,7 +3,7 @@ import mysql from 'mysql2';
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'mysqlpw',
+    password: '<insert your own pw here>',
     database: 'bike_erp'
 });
 
