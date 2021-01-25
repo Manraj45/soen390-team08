59b4e5adf12bb328d0bf375a0dc4a12acd2fef60
Simon Lim <simonlim898@hotmail.com>
2021-01-24 19:00:55 -0500

    #14 added dist to gitignore

--- bike-erp/backend/dist/main/sum.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-let myAdd = function (x, y) {
-    return x + y;
-};
-module.exports = myAdd;
-console.log(myAdd(1, 7));
--- bike-erp/backend/dist/test/sum.test.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const sum = require('../main/sum');
-test('should add 2 number', () => {
-    expect(sum(1, 2)).toBe(3);
-});
