bfd1b0778e737dd13a6bb9fa2a39692ec806afe5
Sam <huangs.wk.0508@gmail.com>
2021-02-02 16:23:25 -0500

    Login authentication update

--- bike-erp/backend/src/main/config/config.ts
+++ bike-erp/backend/src/main/config/config.ts
@@ -1,3 +1,3 @@
-export const DB_PASSWORD: string = "Enter your password"
+export const DB_PASSWORD: string = "Password"
 export const BACKEND_PORT: number = 3000
 export const AUTH_PORT: number = 4000
