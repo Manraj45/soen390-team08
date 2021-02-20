b9ca726d2e37e7aa9c51eed69be9cb07b59b7f1b
Simon Lim <simonlim898@hotmail.com>
2021-02-19 21:44:43 -0500

    #09 Updated remote db credential

--- bike-erp/backend/src/main/config/config.ts
+++ bike-erp/backend/src/main/config/config.ts
@@ -3,9 +3,9 @@ export const BACKEND_PORT: number = 3001
 export const AUTH_PORT: number = 4000
 
 export const DB_HOST: string = "sql5.freemysqlhosting.net"
-export const DB_USER: string = "sql5392428"
-export const DB_PASSWORD: string = "7Vg5f3LF5I"
-export const DB_DATABASE: string = "sql5392428"
+export const DB_USER: string = "sql5394076"
+export const DB_PASSWORD: string = "T3sEvcBBGl"
+export const DB_DATABASE: string = "sql5394076"
 export const DB_PORT: number = 3306
 
 //Local database
