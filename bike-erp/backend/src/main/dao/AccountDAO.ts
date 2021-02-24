2da2677f4fe70d3be72f5636812673b9f20d1f62
Sam <huangs.wk.0508@gmail.com>
2021-02-23 23:18:25 -0500

    Merged and resolved conflicts from development

--- bike-erp/backend/src/main/dao/AccountDAO.ts
+++ bike-erp/backend/src/main/dao/AccountDAO.ts
@@ -1,30 +1,59 @@
-import db from '../helpers/db';
+import db from "../helpers/db";
 
 export class AccountDao {
+  public fetchAccount = (email: string) => {
+    //Getting all the user information from the database
+    return new Promise<Array<any>>((resolve, reject) => {
+      const query = 'SELECT * FROM `account` WHERE email="' + email + `"`;
+      db.query(query, (err, rows) => {
+        if (err) return reject(err);
+        resolve(JSON.parse(JSON.stringify(rows)));
+      });
+    });
+  };
 
-    public fetchAccount = (email: string) => {
-        //Getting all the user information from the database
-        return new Promise<Array<any>>((resolve, reject) => {
-            const query = 'SELECT * FROM `account` WHERE email="' + email + `"`;
-            db.query(query, (err, rows) => {
-                if (err) return reject(err);
-                resolve(JSON.parse(JSON.stringify(rows)));
-            });
-        });
-    }
-
-    public createAcccount = (firstName: string, lastName: string, role: string, password: string, email: string, recovery_question1: string, recovery_question1_answer: string, recovery_question2: string, recovery_question2_answer: string, organization: string) => {
-        return new Promise<any>((resolve, rejects) => {
-            const insert = 'INSERT INTO `account` (`first_name`, `last_name`, `role`, `password`, `email`, `recovery_question1`, `recovery_question1_answer`, `recovery_question2`, `recovery_question1_2`, `organization`) VALUES (\'' + firstName + '\', \'' + lastName + '\', \'' + role + '\', \'' + password + '\', \'' + email + '\', \'' + recovery_question1 + '\', \'' + recovery_question1_answer + '\', \'' + recovery_question2 + '\', \'' + recovery_question2_answer + '\', \'' + organization + '\');';
-            db.query(insert, (err) => {
-                if (err) {
-                    rejects(err);
-                }
-                else {
-                    resolve({ message: "Record inserted succesfully." });
-                }
-            });
-        });
-    }
-
+  public createAcccount = (
+    firstName: string,
+    lastName: string,
+    role: string,
+    password: string,
+    email: string,
+    recovery_question1: string,
+    recovery_question1_answer: string,
+    recovery_question2: string,
+    recovery_question2_answer: string,
+    organization: string
+  ) => {
+    return new Promise<any>((resolve, rejects) => {
+      const insert =
+        "INSERT INTO `account` (`first_name`, `last_name`, `role`, `password`, `email`, `recovery_question1`, `recovery_question1_answer`, `recovery_question2`, `recovery_question1_2`, `organization`) VALUES ('" +
+        firstName +
+        "', '" +
+        lastName +
+        "', '" +
+        role +
+        "', '" +
+        password +
+        "', '" +
+        email +
+        "', '" +
+        recovery_question1 +
+        "', '" +
+        recovery_question1_answer +
+        "', '" +
+        recovery_question2 +
+        "', '" +
+        recovery_question2_answer +
+        "', '" +
+        organization +
+        "');";
+      db.query(insert, (err) => {
+        if (err) {
+          rejects(err);
+        } else {
+          resolve({ message: "Record inserted succesfully." });
+        }
+      });
+    });
+  };
 }
