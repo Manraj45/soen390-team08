5515b10e53d260d883e232879ddae9f6a9d48a7f
Simon Lim <simonlim898@hotmail.com>
2021-03-07 23:43:02 -0500

    #91 Added endpoint for retrieving account payable and transaction items

--- bike-erp/backend/src/main/routes/accouting_routes.ts
+++ bike-erp/backend/src/main/routes/accouting_routes.ts
@@ -1,7 +1,39 @@
 import express from "express";
 import { AccountingService } from "../services/accountingService/AccountingService";
+import { AuthenticationService } from "../services/authenticationService/AuthenticationService";
 
 const router = express();
 
 //Creating a singleton instance of the AccountingService
 AccountingService.getAccountingService()
+
+router.get('/accountPayables', (req, res) => {
+    //Setting the endpoint header to authorization
+    const authHeader = req.headers["authorization"];
+
+    //setting token header
+    const token = authHeader && authHeader.split(" ")[1];
+    const userEmail = AuthenticationService.retrieveAccountFromToken(token);
+
+    AccountingService.getAccountPayablesForUser(userEmail).then((response) => {
+        res.json(response);
+    }).catch(error => {
+        res.status(error.status).send(error.message);
+    })
+})
+
+router.get('/accountPayables/:id/transactionItem', (req, res) => {
+    const id = Number(req.params.id)
+    // Check for validity of account payable id passed. Must be an int
+    if (!Number.isInteger(id)) {
+        res.status(400).send({ message: "Invalid Account Payable ID" })
+    } else {
+        AccountingService.getTransactionItemsByAccountPayable(id)?.then(response => {
+            res.json(response);
+        }).catch(error => {
+            res.status(error.status ? error.status : 500).send({ message: error.sqlMessage ? error.sqlMessage : error.message });
+        })
+    }
+})
+
+export default router;
--- bike-erp/backend/src/main/services/accountingService/AccountingService.ts
+++ bike-erp/backend/src/main/services/accountingService/AccountingService.ts
@@ -4,8 +4,10 @@ export class AccountingService {
 
     private static accountingService: AccountingService | undefined;
     private static accountPayableDAO: AccountPayableDAO | undefined;
+
     private constructor() { AccountingService.accountPayableDAO = new AccountPayableDAO() }
 
+    // Instanciating singleton
     public static getAccountingService() {
         if (this.accountingService === undefined) {
             this.accountingService = new AccountingService();
@@ -14,15 +16,20 @@ export class AccountingService {
         }
     }
 
+    // Method for handling the creating of a account payable
     public static async createAccountPayable(orderList: Array<Order>, email: string) {
         let total = 0;
 
+        // Calculate total
         orderList.forEach((order) => {
             total = order.price * order.quantity;
         })
 
         try {
+            // Creating account payable in db
             const accountPayableId = await AccountingService.accountPayableDAO?.createAccountPayable(total, new Date().toISOString().slice(0, 19).replace('T', ' '), email) as number;
+            
+            // Create transaction item in db for each item in order list
             orderList.forEach(async (order) => {
                 const transactionItemId = await AccountingService.accountPayableDAO?.createTransactionItems(order.price * order.quantity, order.id, order.quantity) as number;
                 await AccountingService.accountPayableDAO?.createConsistOf(accountPayableId, transactionItemId);
@@ -33,6 +40,21 @@ export class AccountingService {
         catch (error) {
             throw { status: 500, message: error.sqlMessage };
         }
+    }
+
+    public static getAccountPayablesForUser(email: string) {
+        return new Promise((resolve, rejects) => {
+            try {
+                const accountPayableList = AccountingService.accountPayableDAO?.getAccountPayableByEmail(email);
+                resolve(accountPayableList);
+            } catch (error) {
+                rejects({ status: 500, message: error.sqlMessage });
+            }
+        })
+    }
+
+    public static getTransactionItemsByAccountPayable(accountPayableId: number) {
+        return AccountingService.accountPayableDAO?.getTransactionByAccountPayableID(accountPayableId);
     }
 }
 export interface Order {
