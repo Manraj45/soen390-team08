f01f6a276845a7ccf1b0b203a7e1ae5d3796bd29
Sam <huangs.wk.0508@gmail.com>
2021-03-10 18:19:52 -0500

    Added case where first user registers with ADMIN role

--- bike-erp/backend/src/main/config/config.ts
+++ bike-erp/backend/src/main/config/config.ts
@@ -7,8 +7,8 @@ export const DB_PASSWORD: string = "T3sEvcBBGl";
 export const DB_PORT: number = 3306;
 export const DB_USER: string = "sql5394076";
 
-/*export const DB_HOST: string = "localhost"
-export const DB_USER: string = "root"
-export const DB_PASSWORD: string = "Enter your password"
-export const DB_DATABASE: string = "bike_erp"
-export const DB_PORT: number = 3306*/
+// export const DB_HOST: string = "localhost";
+// export const DB_USER: string = "root";
+// export const DB_PASSWORD: string = "Enter your password";
+// export const DB_DATABASE: string = "bike_erp";
+// export const DB_PORT: number = 3306;
