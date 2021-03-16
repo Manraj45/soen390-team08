86588f670a9c2fc4efd7a6d4e79b4a5142a766dc
Lauren Lim <mnxnhl@gmail.com>
2021-03-16 19:33:59 -0400

    #119 adding snapshot test for frontend of bike order

---
+++ bike-erp/src/test/OrderBikePage/orderBike.test.tsx
@@ -0,0 +1,40 @@
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import { BrowserRouter } from "react-router-dom";
+
+import renderer from "react-test-renderer";
+import OrderBike from "../../components/OrderBike/OrderBike";
+import store from "../../redux/store";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+const setUp = () => {
+    const testStore = store;
+    const wrapper = shallow(<OrderBike store={testStore} />)
+      .childAt(0)
+      .dive();
+    return wrapper;
+  };
+
+describe("Order Bike", () => {
+    let wrapper: any;
+
+    beforeEach(() => {
+        wrapper = setUp();
+    });
+
+    it("inventory render", () => {
+        expect(wrapper.exists()).toBe(true);
+    });
+
+    it("match rendered component with snapshot", () => {
+        const tree = renderer.create(
+          <BrowserRouter>
+            <OrderBike store={store} />
+            ).toJSON();
+          </BrowserRouter>
+        );
+        expect(tree).toMatchSnapshot();
+      });
+
+});
