9c5422947ba1c89fca6a4913b290d4b224179959
Lauren Lim <mnxnhl@gmail.com>
2021-03-16 17:46:06 -0400

    #119 adding backend test for order bike service

---
+++ bike-erp/backend/src/test/services/bikeOrderService/bikeOrderService.test.ts
@@ -0,0 +1,90 @@
+import db from "../../../main/helpers/db";
+import { BikeOrderService } from "../../../main/services/orderService/BikeOrderService";
+
+describe("Bike Order Test", () => {
+
+    beforeAll(async () => {
+        //Creating the singleton instance of bike order service
+        await BikeOrderService.getBikeOrderService();
+    });
+    // destroy connection after the test
+    afterAll(() => {
+        //Closing database connection
+        db.end();
+    });
+
+    test("Order a bike", async () => {
+        let arrayOrder: any[] = [];
+        let email = "test@test.com";
+        arrayOrder.push({
+            price: 2, size: "MEDIUM", color: "blue", bike_description: "mike", grade: "chrome",
+            quantity: 75, handle_id: 35, wheel_id: 41, frame_id: 120, seat_id: 68, drive_train_id: 86
+        })
+        return expect(
+            BikeOrderService.addBike(arrayOrder, email)
+        ).resolves.toEqual({
+            status: 201,
+            message: "Bike was sold succesfully",
+        });
+    });
+
+    test("Negative quantity", async () => {
+        let arrayOrder: any[] = [];
+        let email = "test@test.com";
+        arrayOrder.push({
+            price: 2, size: "MEDIUM", color: "blue", bike_description: "mike", grade: "chrome",
+            quantity: -23, handle_id: 35, wheel_id: 41, frame_id: 120, seat_id: 68, drive_train_id: 86
+        })
+        return expect(
+            BikeOrderService.addBike(arrayOrder, email)
+        ).rejects.toEqual({
+            status: 400,
+            message: "Invalid quantity format, quantity must be a number data type and a positive number",
+        });
+    });
+
+    test("Not an integer quantity", async () => {
+        let arrayOrder: any[] = [];
+        let email = "test@test.com";
+        arrayOrder.push({
+            price: 25, size: "MEDIUM", color: "blue", bike_description: "mike", grade: "chrome",
+            quantity: "asdasd", handle_id: 35, wheel_id: 41, frame_id: 120, seat_id: 68, drive_train_id: 86
+        })
+        return expect(
+            BikeOrderService.addBike(arrayOrder, email)
+        ).rejects.toEqual({
+            status: 400,
+            message: "Invalid quantity format, quantity must be a number data type and a positive number",
+        });
+    });
+
+    test("Negative price", async () => {
+        let arrayOrder: any[] = [];
+        let email = "test@test.com";
+        arrayOrder.push({
+            price: -1230, size: "MEDIUM", color: "blue", bike_description: "mike", grade: "chrome",
+            quantity: 2, handle_id: 35, wheel_id: 41, frame_id: 120, seat_id: 68, drive_train_id: 86
+        })
+        return expect(
+            BikeOrderService.addBike(arrayOrder, email)
+        ).rejects.toEqual({
+            status: 400,
+            message: "Invalid price format, price must be a number data type and a positive number",
+        });
+    });
+
+    test("Not an integer price", async () => {
+        let arrayOrder: any[] = [];
+        let email = "test@test.com";
+        arrayOrder.push({
+            price: "asd", size: "MEDIUM", color: "blue", bike_description: "mike", grade: "chrome",
+            quantity: -23, handle_id: 35, wheel_id: 41, frame_id: 120, seat_id: 68, drive_train_id: 86
+        })
+        return expect(
+            BikeOrderService.addBike(arrayOrder, email)
+        ).rejects.toEqual({
+            status: 400,
+            message: "Invalid price format, price must be a number data type and a positive number",
+        });
+    });
+});
