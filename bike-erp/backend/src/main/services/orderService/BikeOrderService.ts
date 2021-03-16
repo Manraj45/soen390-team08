cfb127556fce291726f0b1feecee7da3b2b620a2
Lauren Lim <36893250+Mnx458@users.noreply.github.com>
2021-03-16 15:22:55 -0400

    #89 Implementing bike order feature (#124)
    * #77 adding remove option for order list
    
    * #78 adding scrolling feature on order
    
    * #80 #81 adding price,label and error message in order page
    
    * adding post qpis for bike creation
    
    * #101 implementing backend for orderBike
    
    * fixing linter
    
    * #77 adding remove option for order list
    
    * #78 adding scrolling feature on order
    
    * #80 #81 adding price,label and error message in order page
    
    * adding post qpis for bike creation
    
    * #101 implementing backend for orderBike
    
    * fixing linter
    
    * fixing conflicts
    
    * changing bike route name
    
    * resolving changes
    
    * adding comments
    
    * Implement front-end of order bike
    
    * Add redux to front-end
    
    * #101 adding API for bike compose_of creation and quantity edit on sale
    
    * Fix errors and remove unsused code in bike order
    
    * #89 adding redux for front end of bike order
    
    * #89 adding purchase bike feature
    
    * #89 modifying display of bike order
    
    * #89 Add inventory quantity to order bike page
    
    * #89 Add existing bike and quantity check logic
    
    * #89 Add alerts for errors related to bike order
    
    * #89 Add logic to remove bought components from inventory
    
    * #89 Fix total price not updating and implement toggle buttons
    
    * #89 Fix authentication validation to order bike
    
    * #89 adding title for bike ordre page
    
    * adding comments
    
    * adding semi-columns
    
    * #89 refactoring and modifying front-end title
    
    Co-authored-by: bowsong <song.bowen1@gmail.com>

--- bike-erp/backend/src/main/services/orderService/BikeOrderService.ts
+++ bike-erp/backend/src/main/services/orderService/BikeOrderService.ts
@@ -1,13 +1,20 @@
 import { BikeDao } from "../../dao/BikeDao";
+import { AccountingService } from "../accountingService/AccountingService";
+
 
 export class BikeOrderService {
     private static bikeOrderService: BikeOrderService | undefined;
 
     //restrict so that the service cannot be constructed outside of the class. For singleton pattern
-    private constructor() {}
+    private constructor() { 
+        //Calling singleton of accountingService
+        AccountingService.getAccountingService();
+    }
 
     //Creating statuc instance of the BikeDao Class
     private static bikeDao = new BikeDao();
+
+    
 
     //Getting the bikeDao
     public static getBikeDao = () => {
@@ -17,15 +24,16 @@ export class BikeOrderService {
     // Create/verify the existence of an instance of the bikeOrderService
     public static getBikeOrderService() {
         if (this.bikeOrderService === undefined) {
-        this.bikeOrderService = new BikeOrderService();
+            this.bikeOrderService = new BikeOrderService();
         } else {
-        return this.bikeOrderService;
+            return this.bikeOrderService;
         }
     }
 
-    public static addBike = (bikeOrderList: Array<any>) => {
+    public static addBike = async (bikeOrderList: Array<any>, email : string) => {
         return new Promise(async (resolve, rejects) => {
-            bikeOrderList.forEach(bike => {
+            let bikeIdList: number[] = []
+            bikeOrderList.forEach(async bike => {
                 //verifying the data meets the requirements for the price
                 if (isNaN(bike.price) || bike.price < 0) {
                     return rejects({ status: 400, message: "Invalid price format, price must be a number data type and a positive number" });
@@ -34,10 +42,17 @@ export class BikeOrderService {
                 else if (isNaN(bike.quantity) || bike.quantity < 0) {
                     return rejects({ status: 400, message: "Invalid quantity format, quantity must be a number data type and a positive number" });
                 }
-                    BikeOrderService.getBikeDao().createBike(bike.price, bike.size, bike.color, bike.description, bike.grade, bike.quantity)
-                    .catch(error => {
-                        return rejects(error.message)
-                    })
+                //Posting all the bikes created with their components Id.
+                try {
+                    const response = await BikeOrderService.getBikeDao().createBike(bike.price, bike.size, bike.color, bike.description, bike.grade, bike.quantity);
+                    await BikeOrderService.getBikeDao().linkBikeToComponents(response.bikeId, bike.handle_id, bike.wheel_id, bike.frame_id, bike.seat_id, bike.drive_train_id);
+                    bikeIdList.push(response.bikeId);
+                    await AccountingService.createAccountReceivable(bikeOrderList, bikeIdList, email);
+
+                }
+                catch (error) {
+                    return rejects({ status: 500, message: error.sqlMessage })
+                }
             })
             return resolve({ status: 201, message: "Bike was sold succesfully" })
         })
