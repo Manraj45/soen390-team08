475010fbc27d02a27f68cf0f6826051fd9fe7b40
Sam <huangs.wk.0508@gmail.com>
2021-03-16 19:06:21 -0400

    Updated guarding for permission page

--- bike-erp/src/components/SideDrawer/SideDrawer.tsx
+++ bike-erp/src/components/SideDrawer/SideDrawer.tsx
@@ -1,34 +1,49 @@
-import { List, ListItem, Typography } from '@material-ui/core';
+import { List, ListItem, Typography } from "@material-ui/core";
 import { Link } from "react-router-dom";
 import useStyles from "./SideDrawerStyle";
+import { connect } from "react-redux";
 
 /*Drawer on the left side that appears when we open the menu*/
 
-const SideDrawer = () => {
+const SideDrawer = ({ account }: any) => {
+  const style = useStyles();
 
-    const style = useStyles();    
+  return (
+    <nav className={style.drawer}>
+      <List>
+        <Link to="/orderbike" style={{ textDecoration: "none" }}>
+          <ListItem className={style.menuItems}>
+            <Typography>Order Bike</Typography>
+          </ListItem>
+        </Link>
+        <Link to="/order" style={{ textDecoration: "none" }}>
+          <ListItem className={style.menuItems}>
+            <Typography>Order Components</Typography>
+          </ListItem>
+        </Link>
+        <Link to="/inventory" style={{ textDecoration: "none" }}>
+          <ListItem className={style.menuItems}>
+            <Typography>Inventory</Typography>
+          </ListItem>
+        </Link>
+        {account.account.role === "ADMIN" ? (
+          <Link to="/admin" style={{ textDecoration: "none" }}>
+            <ListItem className={style.menuItems}>
+              <Typography>Permissions</Typography>
+            </ListItem>
+          </Link>
+        ) : (
+          <></>
+        )}
+      </List>
+    </nav>
+  );
+};
 
-    return (
-        <nav className={style.drawer}>
-            <List>
-                <Link to="/orderbike" style={{ textDecoration: 'none' }}>
-                    <ListItem className={style.menuItems}>
-                        <Typography>Order Bike</Typography>
-                    </ListItem>
-                </Link>
-                <Link to="/order" style={{ textDecoration: 'none' }}>
-                    <ListItem className={style.menuItems}>
-                        <Typography>Order Components</Typography>
-                    </ListItem>
-                </Link>
-                <Link to="/inventory" style={{ textDecoration: 'none' }}>
-                    <ListItem className={style.menuItems}>
-                        <Typography>Inventory</Typography>
-                    </ListItem>
-                </Link>
-            </List>
-        </nav>
-    );
-}
+const mapStateToProps = (state: any) => {
+  return {
+    account: state.account,
+  };
+};
 
-export default SideDrawer;
+export default connect(mapStateToProps)(SideDrawer);
