3e3b1c8f2f9cdee557e3967e62c1ef3f3f9d60ff
David Thai <davidthai99@gmail.com>
2021-03-17 17:17:13 -0400

    Updated comments for UserLogs component

--- bike-erp/src/components/UserLogs/UserLogs.tsx
+++ bike-erp/src/components/UserLogs/UserLogs.tsx
@@ -7,6 +7,7 @@ import Axios from "axios";
 import React, { useEffect, useState } from "react";
 import useStyles from "../Inventory/inventoryStyles";
 
+// Component for UserLogs page
 const UserLogs: React.FC = () => {
     const [logTable, setlogTable] = useState<any[]>([]);
 
