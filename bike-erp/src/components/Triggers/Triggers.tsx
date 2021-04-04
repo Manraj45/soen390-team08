9412617fdfd94e7b5ec41ce919aa7a47ca476f83
David Thai <davidthai99@gmail.com>
2021-04-04 19:02:51 -0400

    changed commits for Triggers

---
+++ bike-erp/src/components/Triggers/Triggers.tsx
@@ -0,0 +1,74 @@
+// DEPENDENCIES
+import Axios from "axios";
+import React, { useEffect, useState } from "react";
+
+// SERVICES
+import { BACKEND_URL } from "../../core/utils/config";
+
+// STYLING
+import {
+  Switch,
+  FormGroup,
+  FormControlLabel,
+  FormControl,
+} from "@material-ui/core";
+import useStyles from "./TriggersStyles";
+
+/*
+  The Triggers component.
+  Admins, managers and employees can view triggers and activate/deactivate them.
+*/
+const Triggers: React.FC = () => {
+  const styles = useStyles();
+  const url = BACKEND_URL;
+
+  const [triggers, setTriggers] = useState<any[]>([]);
+
+  useEffect(() => {
+    Axios.get(`${url}/triggers/`).then((response) => {
+      setTriggers(response.data);
+    });
+  }, [url]);
+
+  const handleChange = async (id: string) => {
+    await Axios.put(`${url}/triggers/toggle/` + id).catch((error) => {
+    });
+    Axios.get(`${url}/triggers/`).then((response) => {
+      setTriggers(response.data);
+    });
+  };
+
+  return (
+    <React.Fragment>
+      <div id="userLogsPage" className={styles.background}>
+        <br></br>
+        <div className={styles.title}>Triggers</div>
+        <br></br>
+        <div>
+          <FormControl>
+            <FormGroup>
+              {console.log(triggers)}
+              {triggers.map((trigger) => (
+                <FormControlLabel
+                  key={trigger.trigger_id}
+                  value={trigger.trigger_type}
+                  control={
+                    <Switch
+                      color="primary"
+                      checked={Boolean(trigger.activated)}
+                      onChange={() => handleChange(trigger.trigger_id)}
+                    />
+                  }
+                  label={trigger.trigger_type}
+                  labelPlacement="start"
+                />
+              ))}
+            </FormGroup>
+          </FormControl>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Triggers;
