6b6d91da9e9f5ca2d0b0075671787cd68dcf8ec1
Manraj Rai <manraj14@icloud.com>
2021-04-12 15:03:51 -0400

    #182 Fixed issue where backend connection with database dies after some time

--- bike-erp/backend/src/main/helpers/db.ts
+++ bike-erp/backend/src/main/helpers/db.ts
@@ -1,23 +1,19 @@
 import mysql from "mysql2";
 import {
-    DB_HOST,
-    DB_USER,
-    DB_PASSWORD,
-    DB_DATABASE,
-    DB_PORT,
+  DB_HOST,
+  DB_USER,
+  DB_PASSWORD,
+  DB_DATABASE,
+  DB_PORT,
 } from "../config/config";
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
   database: DB_DATABASE,
   port: DB_PORT,
   dateStrings: true
-});
-
-db.connect(function (err) {
-    if (err) throw err;
 });
 
 export default db;
